b4f892305156209ec283e1fc3e4e83803a37d973
Roger Espinosa <roger@umich.edu>
2024-03-21 15:40:40 -0400

    refine the panel toolbar

--- src/js/components/ImageViewerHeader.svelte
+++ src/js/components/ImageViewerHeader.svelte
@@ -13,32 +13,38 @@
 <div class="header flex flex-flow-row flex-align-center flex-space-between">
   <span>{manifest?.getLabel()?.getValue()}</span>
   <div class="header--controls flex flex-flow-row flex-align-center justify-end">
-    <div class="button--group">
-      <sl-tooltip content="Toggle pages">
-        <button class="button button--ghost border-rounded-left" 
-          class:active={panelTabs.pages}
-          on:click={() => { panelTabs.pages = ! panelTabs.pages; }}>
-          <span class="material-icons">view_list</span>
-          <span>Pages</span>
-        </button>
-      </sl-tooltip>
-      <sl-tooltip content="Toggle image">
-      <button class="button button--ghost" 
-        class:active={panelTabs.image}
-        on:click={() => { panelTabs.image = ! panelTabs.image; if ( panelTabs.plaintext && panelTabs.image && viewerWidth < 800 ) { panelTabs.plaintext = false; } }}>
-        <span class="material-icons">image</span>
-        <span>Image</span>
-      </button>
-      </sl-tooltip>
-      <sl-tooltip content="Toggle text">
-      <button class="button button--ghost border-rounded-right" 
-        class:active={panelTabs.plaintext}
-        on:click={() => { panelTabs.plaintext = ! panelTabs.plaintext; if ( panelTabs.image && panelTabs.plaintext && viewerWidth < 800 ) { panelTabs.image = false; } }}
-        disabled={!hasPageText}>
-        <span class="material-icons">article</span>
-        <span>Text</span>
-      </button>
-      </sl-tooltip>
+    <div class="toggle--group">
+      <div class="toggle" class:toggled={panelTabs.pages}>
+        <sl-tooltip content="Toggle pages" data-active={panelTabs.pages}>
+          <button class="button button--ghost" 
+            aria-pressed={panelTabs.pages.toString()}
+            on:xclick={() => { panelTabs.pages = ! panelTabs.pages; }}>
+            <span class="material-icons">view_list</span>
+            <span>Pages</span>
+          </button>
+        </sl-tooltip>
+      </div>
+      <div class="toggle" class:toggled={panelTabs.image}>
+        <sl-tooltip content="Toggle image" data-active={panelTabs.image}>
+          <button class="button button--ghost"
+            aria-pressed={panelTabs.image.toString()}
+            on:click={() => { panelTabs.image = ! panelTabs.image; if ( panelTabs.plaintext && panelTabs.image && viewerWidth < 800 ) { panelTabs.plaintext = false; } }}>
+            <span class="material-icons">image</span>
+            <span>Image</span>
+          </button>
+        </sl-tooltip>
+      </div>
+      <div class="toggle" class:toggled={panelTabs.plaintext}>
+        <sl-tooltip content="Toggle text" data-active={panelTabs.plaintext}>
+          <button class="button button--ghost border-rounded-right" 
+            aria-pressed={panelTabs.plaintext.toString()}
+            on:click={() => { panelTabs.plaintext = ! panelTabs.plaintext; if ( panelTabs.image && panelTabs.plaintext && viewerWidth < 800 ) { panelTabs.image = false; } }}
+            disabled={!hasPageText}>
+            <span class="material-icons">article</span>
+            <span>Text</span>
+          </button>
+        </sl-tooltip>
+      </div>
     </div>
     <slot name="fullscreen"></slot>
   </div>
@@ -51,5 +57,45 @@
     box-shadow: 0px 4px 15px -3px rgba(0,0,0,0.5);
     border-top: 2px solid var(--color-teal-300);
     z-index: 10;
-  }  
+  }
+
+  .toggle--group {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  }
+
+  .toggle {
+    border: 1px solid var(--color-neutral-500);
+  }
+
+  .toggle:first-child {
+    border-top-left-radius: var(--radius-default);
+    border-bottom-left-radius: var(--radius-default);
+    /* border-right-width: 0; */
+  }
+
+  .toggle + .toggle {
+    border-left-width: 0;
+  }
+
+  .toggled {
+    background: var(--color-teal-100);
+  }
+
+  .toggle:last-child {
+    border-top-right-radius: var(--radius-default);
+    border-bottom-right-radius: var(--radius-default);
+    /* border-left-width: 0; */
+  }
+
+  .button--ghost {
+    border: none;
+    margin: 0;
+    background: transparent;
+  }
+
+  .toggle:hover, .toggle:focus-within {
+    z-index: 1;
+  }
 </style>
