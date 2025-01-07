501b6c82717b662ca97e18d38038dd562d1b8774
Roger Espinosa <roger@umich.edu>
2025-01-07 08:52:44 -0500

    build all scripts

--- scripts/svelte-build.mjs
+++ scripts/svelte-build.mjs
@@ -2,19 +2,22 @@ import esbuild from "esbuild";
 import sveltePlugin from "esbuild-svelte";
 import { argv, exit } from 'node:process';
 
-const inputFilename = argv.at(-1);
-const outputFilename = inputFilename.replace('src/', 'dist/');
+// const inputFilename = argv.at(-1);
+argv.slice(2).forEach((inputFilename) => {
+  const outputFilename = inputFilename.replace('src/', 'dist/');
 
-esbuild
-  .build({
-    entryPoints: [inputFilename],
-    mainFields: ["svelte", "browser", "module", "main"],
-    conditions: ["svelte", "browser"],
-    bundle: true,
-    format: 'iife',
-    globalName: 'DLXS.viewer',
-    outfile: outputFilename,
-    plugins: [sveltePlugin({compilerOptions: { css: true }})],
-    logLevel: "info",
-  })
-  .catch(() => process.exit(1));
+  esbuild
+    .build({
+      entryPoints: [inputFilename],
+      mainFields: ["svelte", "browser", "module", "main"],
+      conditions: ["svelte", "browser"],
+      bundle: true,
+      format: 'iife',
+      globalName: 'DLXS.viewer',
+      outfile: outputFilename,
+      plugins: [sveltePlugin({compilerOptions: { css: true }})],
+      logLevel: "info",
+    })
+    .catch(() => process.exit(1));
+})
+
